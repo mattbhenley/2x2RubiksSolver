e7c36e07623c10efa18c95024cbcc733acaf7b24
Matt Henley <mattbhenley@gmail.com>
2023-11-06 19:40:26 -0600

    create 2d array

---
+++ solver.py
@@ -0,0 +1,20 @@
+cube = [
+    # Front face
+    [['F1', 'F2'],
+     ['F3', 'F4']],
+    # Back face
+    [['B1', 'B2'],
+     ['B3', 'B4']],
+    # Left face
+    [['L1', 'L2'],
+     ['L3', 'L4']],
+    # Right face
+    [['R1', 'R2'],
+     ['R3', 'R4']],
+    # Up face
+    [['U1', 'U2'],
+     ['U3', 'U4']],
+    # Down face
+    [['D1', 'D2'],
+     ['D3', 'D4']]
+]
