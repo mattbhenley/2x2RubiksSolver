c90dfe5c9d40cfe099ccfa6f63fe5fd5c1483fca
Matt Henley <mattbhenley@gmail.com>
2023-11-06 19:42:10 -0600

    added sample cube and apply move

--- solver.py
+++ solver.py
@@ -1,3 +1,5 @@
+import random
+
 cube = [
     # Front face
     [['F1', 'F2'],
@@ -18,3 +20,26 @@ cube = [
     [['D1', 'D2'],
      ['D3', 'D4']]
 ]
+
+def scramble_cube(cube, num_moves):
+    possible_moves = ["U", "U'", "R", "R'", "F", "F'"]
+    for _ in range(num_moves):
+        move = random.choice(possible_moves)
+        apply_move(cube, move)
+
+def apply_move(cube, move):
+    # Implement the logic to apply the move to the cube.
+
+
+    def solve_cube(cube):
+    # Solve the first layer.
+    solve_first_layer(cube)
+
+    # Solve the second layer.
+    solve_second_layer(cube)
+
+    # Check if the cube is solved.
+    if is_solved(cube):
+        print("Cube is solved!")
+    else:
+        print("Cube is not solved.")
